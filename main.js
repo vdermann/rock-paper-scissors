161ba5e0545e70ec316dc2df3e5505478ba461cd
Lucas Aguayo <lucasaguayorr@gmail.com>
2025-01-03 09:20:58 -0300

    Wrote the pseudocode into the HTML file. Made the getComputerChoice() function using the Math.random method.

---
+++ main.js
@@ -0,0 +1,15 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let randomValue = Math.random();
+    console.log(randomValue);
+
+    if (randomValue > 0/3 && randomValue < 1/3) {
+        return "Rock";
+    } else if (randomValue > 1/3 &&  randomValue < 2/3) {
+        return "Paper";
+    } else {
+        return "Scissors"
+    }
+}
